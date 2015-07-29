23a3af7308209819ec211f5386cbc9a5354ed935
chenkehxx <chenkehxx@126.com>
2015-07-29 11:05:35 +0800

    build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,12 @@
 var Util = require('./util');
+var _ = require('underscore');
+var $ = require('jquery');
+
+console.log(_);
+console.log($);
+window.$ = $;
+window._ = _;
+
 console.log('I miss you');
 var tools = {
 	extend: 'afe',
