74bac89eb1c9e1215db8f05d99b97175c4a267ee
chenke <chenkehxx@126.com>
2015-10-09 15:03:35 +0800

    接受来自命令行的argv, 自动识别build.conf.json路径

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@
 var gulp        = require('gulp');
 var runSequence = require('run-sequence');
 var gUtil       = require('gulp-util');
-var config      = require('./config.json');
+// var config      = require('./config.json');
 var fs          = require('fs');
 var path        = require('path');
+var configPath  = process.argv.splice(6)[0] || __dirname;
+configPath      = path.join(configPath, 'build.conf.json');
+var config      = require(configPath);
+global.configPath = configPath;
 
 require('run-sequence').use(gulp);
 
@@ -24,8 +28,7 @@ gulp.task('default', ['dev']);
 
 gulp.task('build', ['clean'], function () {
     global.isProd = true;
-
-    runSequence(['sass'], 'usemin')
+    runSequence(['sass'], 'usemin');
 })
 
 
